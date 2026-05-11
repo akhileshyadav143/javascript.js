dc7030a588ee1b5ca557d866d72ba830f01743b3
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-11 23:09:10 +0530

    js

---
+++ javascript.js/javascript5.js
@@ -0,0 +1,9 @@
+console.log("kyaa");
+// let btn= document.querySelector("button");
+let btn= document.querySelector("#two");
+let body =document.querySelector("body");
+
+btn.addEventListener("click",function(){
+    body.style.backgroundColor="red"
+    
+})
